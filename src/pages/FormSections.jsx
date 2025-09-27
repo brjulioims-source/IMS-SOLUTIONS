e8677c20d78363b9b2b9e1dc342925e490453f3c
brjulioims-source <brjulioims@gmail.com>
2025-09-27 17:05:17 -0500

    feact: se hace modal contrato y se va llenando paso a paso

--- src/pages/FormSections.jsx
+++ src/pages/FormSections.jsx
@@ -3,21 +3,34 @@ import Modal from "../components/Modal";
 import "./FormSections.css";
 
 function FormSections() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [openModal, setOpenModal] = useState(false);
 
-    const closeModal = () => setIsModalOpen(false);
+    const closeModal = () => setOpenModal(false);
 
     return(
         <div>
             {/*Secciones horizontales*/}
             <div className="sections">
-                <div className="section-card" onClick={() => setIsModalOpen("contrato")}>📑 Contrato</div>
-                <div className="section-card" onClick={() => setIsModalOpen("fechas")}>📅 Fechas</div>
-                <div className="section-card" onClick={() => setIsModalOpen("costo")}>💵 Costo</div>
-                <div className="section-card" onClick={() => setIsModalOpen("conyuge")}>👥 Cónyuge</div>
+                <div className="section-card" onClick={() => setOpenModal("contrato")}>📑 Contrato</div>
+                <div className="section-card" onClick={() => setOpenModal("fechas")}>📅 Fechas</div>
+                <div className="section-card" onClick={() => setOpenModal("costo")}>💵 Costo</div>
+                <div className="section-card" onClick={() => setOpenModal("conyuge")}>👥 Cónyuge</div>
             </div>
 
-            💵 Costo
+            {/* Modal Contrato */}
+
+            <Modal isOpen={openModal === "contrato"} onClose={closeModal} title="Formulario de Contrato">
+                <form className="form">
+                    <label>Número de Contrato:
+                        <input type="text" placeholder="Ingrese el número de contrato" />
+                    </label>
+                    <label>Cliente:
+                        <input type="text" placeholder="Ingrese el nombre del cliente" />
+                        <button type="submit">Guardar</button>
+                    </label>
+                </form>
+        
+            </Modal>
         </div>
     )
 }
